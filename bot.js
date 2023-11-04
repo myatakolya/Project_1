8e43e2a624dfbcdca3961a19fac6db5e9b35d8e8
ivan borodkin <ivan.b@devstark.com>
2023-11-05 00:37:47 +0300

    bot wip

--- bot.js
+++ bot.js
@@ -1,16 +1,32 @@
-const TelegramBot = require('node-telegram-bot-api');
-const express = require('express');
-require('dotenv').config();
+import TelegramBot from 'node-telegram-bot-api'
+// import express from 'express'
+import 'dotenv/config'
 
 // const app = express();
-const bot = new TelegramBot(process.env.BOTKEY, {polling: true});
+const bot = new TelegramBot(process.env.TG_API_TOKEN, { polling: true });
 
-bot.onText(/\/start/, msg => {
-  const { chat: {id} } = msg;
-  bot.sendMessage(id, "Начинаем")
+bot.on('text', msg => {
+  console.log(msg)
+});
+
+bot.on('text', msg => {
+  bot.sendMessage(msg.chat.id, `Hi, ${msg.from.first_name}`)
+});
+
+bot.on('text', async msg => {
+
+  const msgWait = await bot.sendMessage(msg.chat.id, 'Wait 5 sec, pls');
+
+  setTimeout( async () => {
+    bot.editMessageText('Yappy', {
+      chat_id: msgWait.chat.id,
+      message_id: msgWait.message_id
+    });
+
+  }, 5000);
 });
 
 
 // app.listen(process.env.PORT, () => console.log(`My server is running on port ${process.env.PORT}`))
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+// process.once("SIGINT", () => bot.stop("SIGINT"));
+// process.once("SIGTERM", () => bot.stop("SIGTERM"));
